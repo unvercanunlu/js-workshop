9550ba56ec31c9ccfdf62375ac7af61e1d055fb0
unver <unvercanunlu@gmail.com>
2025-08-17 01:28:32 +0200

    third commit

---
+++ word-frequency-counter.js
@@ -0,0 +1,72 @@
+
+function removePunctiations(text) {
+    /*
+
+    const empty = ' ';
+
+    return text.replace('.', empty)
+        .replace(',', empty)
+        .replace('?', empty)
+        .replace("'", empty)
+        .replace(':', empty)
+        .replace(';', empty)
+        .replace('!', empty)
+        .replace('"', empty);
+
+    */
+
+    return text.replace(/[.,?':;!]/g, ' ');
+}
+
+function normalize(text) {
+    return removePunctiations(text.toLowerCase())
+}
+
+function countWordFrequency(input) {
+    if (input == null || input === undefined || typeof input !== 'string' || input.length == 0) {
+        throw new Error("input must be non-empty string!");
+    }
+
+    const normalized = normalize(input);
+
+    const splitted = normalized.split(' ')
+        .map(word => word.trim())
+        .filter(word => word.length !== 0);
+
+    let frequencies = new Map();
+
+    for (let word of splitted) {
+        /*
+
+        let count = frequencies.get(word);
+
+        if(!count){
+            count = 0;
+        }
+        count++;
+
+        frequencies.set(word,count);
+
+        */
+
+        frequencies.set(word, (frequencies.get(word) || 0) + 1);
+    }
+
+    /*
+
+    let result = {};
+
+    for (const word of frequencies.keys()) {
+        result[word] = frequencies.get(word);
+    }
+
+    return result;
+
+    */
+
+    return Object.fromEntries(frequencies);
+}
+
+console.log(
+    countWordFrequency("the quick brown fox jumps over the lazy dog")
+);
