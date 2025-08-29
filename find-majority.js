b5fa90083840cf8e37739f1ef7d70901aaf1ae2d
unver <unvercanunlu@gmail.com>
2025-08-29 14:50:59 +0200

    updated.

---
+++ find-majority.js
@@ -0,0 +1,38 @@
+function checkInputInvalid(input) {
+    return input === undefined || input === null
+        || !Array.isArray(input)
+        || input.length === 0;
+}
+
+function checkElementInvalid(element) {
+    return element === undefined || element === null
+        || typeof element !== 'number'
+        || Number.isNaN(element);
+}
+
+
+function findMajority(values) {
+    if (checkInputInvalid(values)) {
+        throw new Error("Input should be non-empty array!");
+    }
+
+    const frequencies = new Map();
+
+    for (const number of values) {
+        if (checkElementInvalid(number)) {
+            console.warn("Element invalid! element=" + number + " skipping...");
+        }
+
+        frequencies.set(number, (frequencies.get(number) || 0) + 1);
+    }
+
+    const threshold = frequencies.size / 2;
+
+    for (const [key, value] of frequencies.entries()) {
+        if (value > threshold) {
+            return key;
+        }
+    }
+
+    return null;
+}
