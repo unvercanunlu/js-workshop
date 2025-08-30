09ff32dad0e4af1c3972c7f3c922e92922fee25d
unver <unvercanunlu@gmail.com>
2025-08-30 20:09:46 +0200

    updated.

--- find-two-sum.js
+++ find-two-sum.js
@@ -1,21 +1,24 @@
 
 
-function checkInputInvalid(input) {
-    return input === undefined || input === null
-        || !Array.isArray(input)
-        || input.length === 0;
+function checkNonEmptyArray(input) {
+    return input !== undefined && input !== null
+        && Array.isArray(input)
+        && input.length !== 0;
 }
 
-function checkElementInvalid(element) {
-    return element === undefined || element === null
-        || typeof element !== 'number'
-        || Number.isNaN(element);
+function checkNonNegativeNumber(element) {
+    return element !== undefined && element !== null
+        && typeof element === 'number'
+        && !Number.isNaN(element);
 }
-
 
 function calculateTwoSum(values, target) {
-    if (checkInputInvalid(values)) {
-        throw new Error("Input should be non-empty array!");
+    if (!checkNonEmptyArray(values)) {
+        throw new Error("First parameter should be non-empty array!");
+    }
+
+    if (!checkNonNegativeNumber(target)) {
+        throw new Error("Second parameter should be number!");
     }
 
     const indexes = new Map();
@@ -23,15 +26,15 @@ function calculateTwoSum(values, target) {
     for (let i = 0; i < values.length; i++) {
         const current = values[i];
 
-        if (checkElementInvalid(current)) {
-            console.warn("Element invalid! element=" + current + " skipping...");
+        if (!checkNonNegativeNumber(current)) {
+            throw new Error("Array elements must be valid numbers!");
         }
 
-        const other = target - current;
-        const otherIndex = indexes.get(other);
+        const complement = target - current;
+        const complementIndex = indexes.get(complement);
 
-        if (otherIndex !== null && otherIndex !== undefined && otherIndex !== i) {
-            return [i, otherIndex];
+        if (complementIndex !== null && complementIndex !== undefined && complementIndex !== i) {
+            return [i, complementIndex];
         }
 
         indexes.set(current, i);
